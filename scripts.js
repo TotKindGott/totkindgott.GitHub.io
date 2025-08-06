88f771c5e24ecd99d384ea7ee1d81c58c9f7cf42
Konstantin A Kolosov <TotKindGott@users.noreply.github.com>
2025-08-06 07:52:24 -0500

    search fix

--- scripts.js
+++ scripts.js
@@ -141,7 +141,7 @@ function parseURL() {
     if (urlParams.has('search') == true) {
         var searchQuery = urlParams.get('search');
         document.getElementById('search_bar').setAttribute("value", searchQuery);
-        searchModels();
+        //searchModels();
         //searchFor(searchQuery);
     };
     if (urlParams.has('mode') == true) {
